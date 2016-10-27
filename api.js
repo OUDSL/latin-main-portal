6c838f60cb70fd6d1801425791594241632fb55c
SriHarsha <sriharsha1507@gmail.com>
2016-10-27 16:20:45 -0500

    Typo --> hearingss corrected

--- api.js
+++ api.js
@@ -240,7 +240,7 @@ function search(term){
                     tot_ret = data.hits.total
                     hear_total = data.aggregations.hearings_count.value
                     if ( hear_total > tot_ret ){hear_total = tot_ret;}
-                    $('.page-content').text( "Search Results: " + tot_ret.toString() + "  Total Hearingss:  " +  hear_total.toString() + ' --- Page ' + page_num + ' of ' + total_pages);
+                    $('.page-content').text( "Search Results: " + tot_ret.toString() + "  Total Hearings:  " +  hear_total.toString() + ' --- Page ' + page_num + ' of ' + total_pages);
                 }
             });
             page=1;
